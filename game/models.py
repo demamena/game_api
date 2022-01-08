5bd908be77432510db4c3f7be9b7046647c26608
demamena <demamenass@gmail.com>
2022-01-08 23:16:49 +0200

    added models

---
+++ game/models.py
@@ -0,0 +1,20 @@
+from django.db.models import Model, TextField, ImageField, DecimalField, DateTimeField, BooleanField, CharField, \
+    FileField
+from django.utils import timezone
+
+
+class Question(Model):
+    name = CharField(max_length=255)
+    question = TextField(max_length=5000)
+    preview = ImageField()
+    rating = DecimalField(default=0, decimal_places=2, max_length=3)
+    add_date = DateTimeField(default=timezone.now)
+    is_hidden = BooleanField(default=True)
+    video = FileField()
+    time_code = CharField(max_length=15, default="0:00")
+
+
+class Answer(Model):
+    answer = TextField(max_length=5000)
+    is_correct = BooleanField(default=False)
+
