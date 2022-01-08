382cc814ca5d266741daeeeb3a8db6c3b74f5bcb
Bitter <vladkastom97@mail.ru>
2022-01-09 01:12:26 +0300

    update

--- game/models.py
+++ game/models.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db.models import Model, TextField, ImageField, DecimalField, DateTimeField, BooleanField, CharField, \
-    FileField
+    FileField, IntegerField, TimeField, ForeignKey, CASCADE
 from django.utils import timezone
 
 
@@ -11,7 +12,7 @@ class Question(Model):
     add_date = DateTimeField(default=timezone.now)
     is_hidden = BooleanField(default=True)
     video = FileField()
-    time_code = CharField(max_length=15, default="0:00")
+    time_code = TimeField(default=0)
 
 
 class Answer(Model):
@@ -19,3 +20,17 @@ class Answer(Model):
     is_correct = BooleanField(default=False)
 
 
+class User(AbstractUser):
+    score = IntegerField(default=0)
+    games_count = IntegerField(default=0)
+
+
+class Game(Model):
+    game_start = DateTimeField(default=timezone.now)
+    game_end = DateTimeField(default=timezone.now)
+
+
+class GameQuestion(Model):
+    game = ForeignKey(Model, on_delete=CASCADE)
+    question = ForeignKey(Question, on_delete=CASCADE)
+    is_answer_correct = BooleanField(default=False)
