8fca1d74875208a11067f3dc4e650d8554c6b1c0
Bitter <vladkastom97@mail.ru>
2022-01-09 02:54:30 +0300

    admin

--- game/admin.py
+++ game/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin import ModelAdmin
 
 from game.models import Question, User, Game, GameQuestion, Answer
 
 
 @admin.register(Question)
-class AdminAdmin(UserAdmin):
+class QuestionAdmin(ModelAdmin):
     list_display = ('id', 'name', 'question', 'preview', 'rating', 'add_date', 'is_hidden', 'video', 'time_code')
     list_display_links = ('id', 'name', 'question', 'rating', 'add_date', 'is_hidden')
     fieldsets = (
-        ('Main', {"fields": ("username", 'full_name', "phone", "picture",
-                             'registration_date')}),
+        ('Main', {"fields": ("name", 'question', "preview", "rating",
+                             'add_date')}),
     )
     readonly_fields = ['id', 'add_date', 'preview']
     search_fields = ('id', 'name', 'question', 'add_date')
